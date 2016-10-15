0c17b30dda1b333f4081989db11558aa96502b8d
Borislav Borisov <borislavborisov22@abv.bg>
2016-10-16 02:40:51 +0300

    adding C# Fundamentals - Introduction to programming homework

---
+++ IntroductionToProgramming/PrintLongSequence/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintLongSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int memberSquence = 2;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(memberSquence);
+                }
+                else
+                {
+                    Console.WriteLine(memberSquence*-1);
+                }
+                memberSquence++;
+            }
+        }
+    }
+}
