6e1d2bf3b222465795869137cca99e78716ec489
Borislav Borisov <borislavborisov22@abv.bg>
2016-10-16 03:06:08 +0300

    Update Program.cs

--- DataTypesAndVariables/IsoscelesTriangles/Program.cs
+++ DataTypesAndVariables/IsoscelesTriangles/Program.cs
@@ -10,7 +10,13 @@ namespace IsoscelesTriangles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine((char));
+            Console.OutputEncoding = Encoding.UTF8;
+            char copyrightSymbol = '\u00A9';
+
+        Console.WriteLine("   {0}",copyrightSymbol);
+        Console.WriteLine("  {0} {0}",copyrightSymbol);
+        Console.WriteLine(" {0} {0} {0}",copyrightSymbol);
+        Console.WriteLine("{0} {0} {0} {0}", copyrightSymbol);
         }
     }
 }
