e276761ef75115e0e10870e5060a18b44ba3bca9
Borislav Borisov <borislavborisov22@abv.bg>
2016-11-24 00:07:27 +0200

    Added Methods, Numeral Systems, Object Workshop Problems

---
+++ Workshop(MethodsNumeralSystemsObjects)/BitShiftMatrix/BitShiftMatrix.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+ 
+class BitShiftMatrix
+{
+    static bool[,] visited;
+    static BigInteger[,] matrix;
+    static void Main()
+    {
+        int rows = int.Parse(Console.ReadLine());
+        int cols = int.Parse(Console.ReadLine());
+        int movementsCount = int.Parse(Console.ReadLine());
+        int[] movements = Console.ReadLine()
+            .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        matrix = FillMatrix(rows, cols);
+        visited = new bool[rows, cols];
+
+        int coefficient = Math.Max(rows, cols);
+        BigInteger totalSum = CalculateTotalSum(movements,coefficient);
+        Console.WriteLine(totalSum);
+        
+    }
+
+    private static BigInteger CalculateTotalSum(int[] movements, int coefficient)
+    {
+        int currRow = matrix.GetLength(0) - 1;
+        int currCol = 0;
+        BigInteger resultSum = 0;
+
+        for (int i = 0;i < movements.Length; i++)
+        {
+            int currentCode = movements[i];
+            int targetRow = currentCode / coefficient;
+            int targetCol = currentCode % coefficient;
+
+            //go to target col and collect number on the way
+            int stepCol = currCol < targetCol ? 1 : -1;
+            while (currCol != targetCol)
+            {
+                if (!visited[currRow, currCol])
+                {
+                    resultSum += matrix[currRow, currCol];
+                    visited[currRow, currCol] = true;
+                }
+
+                currCol += stepCol;
+            }
+
+            //go to target row
+            int stepRow = currRow < targetRow ? 1 : -1;
+
+            while (currRow != targetRow)
+            {
+                if (!visited[currRow, currCol])
+                {
+                    resultSum += matrix[currRow, currCol];
+                    visited[currRow, currCol] = true;
+                }
+
+                currRow += stepRow;
+            }
+
+
+            //check last
+            if (!visited[currRow, currCol])
+            {
+                resultSum += matrix[currRow, currCol];
+                visited[currRow, currCol] = true;
+            }
+        }
+
+
+        return resultSum;
+    }
+
+    private static BigInteger[,] FillMatrix(int rows, int cols)
+    {
+        var resultMatrix = new BigInteger[rows, cols];
+        BigInteger startNumberRow = 1;
+
+        for (int row = rows - 1; row >= 0; row--)
+        {
+            BigInteger numberToFillCol = startNumberRow;
+            for (int col = 0; col < cols; col++)
+            {
+                resultMatrix[row, col] = numberToFillCol;
+                numberToFillCol *= 2;
+            }
+
+            startNumberRow *= 2;
+        }
+
+        return resultMatrix;
+    }
+}
+
---
+++ Workshop(MethodsNumeralSystemsObjects)/Messages/Messages.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+class Messages
+{
+    static List<string> digits = new List<string>(){ "cad", "xoz", "nop", "cyk", "min", "mar", "kon", "iva", "ogi", "yan" };
+    static List<string> digitValues = new List<string>(){ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+    static void Main()
+    {
+        string firstNumberInGeorge = Console.ReadLine();
+        string operation = Console.ReadLine();
+        string secondNumberInGeorge = Console.ReadLine();
+
+        BigInteger firstNumberDecValue = GeorgeToDecimal(firstNumberInGeorge);
+        BigInteger secondNumberDecValue = GeorgeToDecimal(secondNumberInGeorge);
+        
+        BigInteger resultNumberDecimal = OperateWithNumbers(firstNumberDecValue, secondNumberDecValue, operation);
+        
+        string resultInGeorgeSystem = DecimalToGeorge(resultNumberDecimal);
+        Console.WriteLine(resultInGeorgeSystem);
+    }
+
+    private static string DecimalToGeorge(BigInteger decimalValue)
+    {
+        string result = string.Empty;
+
+        while (decimalValue > 0)
+        {
+            BigInteger remainder = decimalValue % 10;
+            result = digits[(int)remainder] + result;
+            decimalValue /= 10;
+        }
+
+        return result;
+
+    }
+
+    private static BigInteger OperateWithNumbers(BigInteger firstNumberDecValue, BigInteger secondNumberDecValue, string operation)
+    {
+        if (operation == "+")
+        {
+            return firstNumberDecValue + secondNumberDecValue;
+        }
+        else
+        {
+            return firstNumberDecValue - secondNumberDecValue;
+        }
+    }
+
+    private static BigInteger GeorgeToDecimal(string inputNumber)
+    {
+        BigInteger result = 0;
+
+        StringBuilder temp = new StringBuilder(inputNumber.Length);
+
+        for (int i = 0; i < inputNumber.Length; i++)
+        {
+            temp.Append(inputNumber[i]);
+
+            if (digits.Contains(temp.ToString()))
+            {
+                int digitIndex = digits.IndexOf(temp.ToString());
+                result = result * 10 + (digitIndex);
+                temp.Clear();
+            }
+            
+        }
+
+        return result;
+    }
+}
+
