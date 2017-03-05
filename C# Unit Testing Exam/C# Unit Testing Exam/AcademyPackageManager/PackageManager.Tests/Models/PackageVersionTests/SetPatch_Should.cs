ee517e867fc016d0f096fa460a56f461182a8c78
Borislav Borisov <borislavborisov22@abv.bg>
2017-03-05 22:56:16 +0200

    added exam solution for the unit testing course

---
+++ C# Unit Testing Exam/C# Unit Testing Exam/AcademyPackageManager/PackageManager.Tests/Models/PackageVersionTests/SetPatch_Should.cs
@@ -0,0 +1,42 @@
+﻿namespace PackageManager.Tests.Models.PackageVersionTests
+{
+    using System;
+
+    using NUnit.Framework;
+    using Enums;
+    using PackageManager.Models;
+
+    [TestFixture]
+    public class SetPatch_Should
+    {
+        [Test]
+        public void ThrowArgumentException_WhenPassedPatchIsInInvalidTRange()
+        {
+            // arrange
+            int major = 1;
+            int minor = 1;
+            int invalidPatch = -3;
+            var versionType = VersionType.alpha;
+
+            // act and assert
+            Assert.Throws<ArgumentException>(() => new PackageVersion(major, minor, invalidPatch, versionType));
+        }
+
+        [Test]
+        public void AssignToPatchCorrectly_WhenPassedPatchRangeIsValid()
+        {
+            // arrange
+            int major = 1;
+            int minor = 2;
+            int validPatch = 3;
+            var versionType = VersionType.alpha;
+
+
+            // act
+            var package = new PackageVersion(major, minor, validPatch, versionType);
+
+            // assert
+            Assert.AreEqual(package.Patch, validPatch);
+        }
+    }
+}
