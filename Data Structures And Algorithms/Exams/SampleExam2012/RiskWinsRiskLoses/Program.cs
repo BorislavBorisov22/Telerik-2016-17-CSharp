ad6eef5b3d66667d0389b3fe79c1e9eafaeea105
Borislav Borisov <borislavborisov22@abv.bg>
2017-08-21 19:23:56 +0300

    Added homeworks and some workshops and demos and cheatsheet fot the Data Strctures Course

---
+++ Data Structures And Algorithms/Exams/SampleExam2012/RiskWinsRiskLoses/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiskWinsRiskLoses
+{
+    public class Program
+    {
+        static string targetCode;
+
+        public static void Main()
+        {
+            var initialCode = Console.ReadLine();
+            targetCode = Console.ReadLine();
+
+            var forbidden = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                forbidden.Add(Console.ReadLine());
+            }
+
+            var result = 0;
+            for (int i = 0; i < initialCode.Length; i++)
+            {
+                result += Math.Abs((initialCode[i] - '0') - (targetCode[i] - '0'));   
+            }
+
+            Console.WriteLine(result);
+        }
+
+        static int min = int.MaxValue;
+
+        public static void Solve(int index, string currentCode, HashSet<string> forbidden, int steps)
+        {
+            if (index >= currentCode.Length)
+            {
+                if (currentCode == targetCode)
+                {
+                    if (steps < min)
+                    {
+                        min = steps;
+                    }
+                }
+
+                return;
+            }
+
+            int currentDigit = currentCode[index] - '0';
+            int targetDigit = currentCode[index] - '0';
+        }
+    }
+}
