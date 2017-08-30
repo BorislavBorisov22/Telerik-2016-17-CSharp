c133c084aebf8d2893a4ddb888be6fa386559169
Borislav Borisov <borislavborisov22@abv.bg>
2017-08-30 21:48:17 +0300

    Added 1,2,5,6 tasks from DSA 2015 exam

---
+++ Data Structures And Algorithms/Exams/Exam2015/BoxFullOfBalls/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxFullOfBalls
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var moves = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            var range = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int start = range[0];
+            int end = range[1];
+
+            var result = 0;
+
+
+            var wins = new bool[end + 1];
+            wins[0] = true;
+            for (int i =1; i <= end; i++)
+            {
+                foreach (var m in moves)
+                {
+                    if (m > i)
+                    {
+                        continue;
+                    }
+
+
+                }
+            }
+
+
+            Console.WriteLine(result);
+        }
+    }
+}
