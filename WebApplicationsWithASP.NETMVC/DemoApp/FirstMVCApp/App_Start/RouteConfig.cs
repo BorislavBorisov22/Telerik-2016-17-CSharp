86623c95d6aaffaa5da6691d8d3853290c106bc6
Borislav Borisov <borislavborisov22@abv.bg>
2017-09-15 18:30:47 +0300

    Added lowercase routes

--- WebApplicationsWithASP.NETMVC/DemoApp/FirstMVCApp/App_Start/RouteConfig.cs
+++ WebApplicationsWithASP.NETMVC/DemoApp/FirstMVCApp/App_Start/RouteConfig.cs
@@ -12,6 +12,7 @@ namespace FirstMVCApp
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.LowercaseUrls = true;
 
             routes.MapRoute(
                 name: "NewsList",
